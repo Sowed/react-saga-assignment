0e59e28fb42f80739cf478a1c4d9ec74c6c74d6c
Aneep Tandel <aneepct@live.com>
2021-09-16 00:40:37 +0400

    First Commit

--- index.js
+++ index.js
@@ -5,8 +5,5 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
-// import axios from 'axios';
-
-// axios.defaults.baseURL = 'https://randomuser.me/api';
 
 AppRegistry.registerComponent(appName, () => App);
